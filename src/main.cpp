334a5c947a9b9f043463c81a80af0bbafc8ab4b7
ICantMakeThings <prigorshnia@proton.me>
2025-02-24 23:51:04 +0100

    blutut  media control - WIP

--- src/main.cpp
+++ src/main.cpp
@@ -65,10 +65,39 @@ char daysOfTheWeek[7][12] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
 Adafruit_BME280 bme; // I2C
 unsigned long delayTime;
 
+unsigned long lastPressTime = 0;
+int clickCount = 0;
+const int multiClickTime = 400;
+
+BLEDis bledis;
+BLEHidAdafruit blehid;
+
 GxEPD2_BW<GxEPD2_290_BS, GxEPD2_290_BS::HEIGHT> display(GxEPD2_290_BS(CS_PIN, DC_PIN, RST_PIN, BUSY_PIN));
 
 void setup() {
   Serial.begin(9600);
+
+  Bluefruit.begin();
+  Bluefruit.setName("BT DEVICE");
+  Bluefruit.Periph.setConnInterval(9, 16);
+  Bluefruit.setTxPower(0);
+
+  bledis.setManufacturer("nor-gate");
+  bledis.setModel("ng-v1");
+  bledis.begin();
+
+  blehid.begin();
+
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+  Bluefruit.Advertising.addService(blehid);
+  Bluefruit.Advertising.addName();
+
+  Serial.println("Adver,");
+  Bluefruit.Advertising.start(0);
+
+
+
 
   unsigned status;
 
@@ -172,6 +201,42 @@ void loop() {
     display.print(" hPa");
 
   } while (display.nextPage());
+/*
+  if (digitalRead(SWEXT) == LOW) {
+    unsigned long pressTime = millis();
+    
+    if (pressTime - lastPressTime > multiClickTime) {
+      clickCount = 1;
+    } else {
+      clickCount++;
+    }
 
+    lastPressTime = pressTime;
+
+    while (digitalRead(SWEXT) == LOW);
+    delay(50);
+  }
+
+  if (millis() - lastPressTime > multiClickTime && clickCount > 0) {
+    if (clickCount == 1) {
+      Serial.println("Pause");
+      blehid.consumerKeyPress(HID_USAGE_CONSUMER_PLAY_PAUSE);
+      delay(100);
+      blehid.consumerKeyRelease();
+    } else if (clickCount == 2) {
+      Serial.println("Next Track");
+      blehid.consumerKeyPress(HID_USAGE_CONSUMER_SCAN_NEXT);
+      delay(100);
+      blehid.consumerKeyRelease();
+    } else if (clickCount == 3) {
+      Serial.println("Previous Track");
+      blehid.consumerKeyPress(HID_USAGE_CONSUMER_SCAN_PREVIOUS);
+      delay(100);
+      blehid.consumerKeyRelease();
+    }
+
+    clickCount = 0;
+  }
+*/
   delay(100);
 }
